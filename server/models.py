3527b8fe354408536359c39c4bfadfd777d31550
Emmanuel Nduhiu <nduhiuemmanuel@gmail.com>
2026-03-06 20:16:56 +0300

    Completed lab

--- server/models.py
+++ server/models.py
@@ -1,16 +1,21 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
-db = SQLAlchemy()
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
 
 class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
-
+    
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
     image = db.Column(db.String)
-    price = db.Column(db.Float)
-    is_in_stock = db.Column(db.Boolean)
-
+    price = db.Column(db.Float, nullable=False)
+    is_in_stock = db.Column(db.Boolean, default=True)
+    
     def __repr__(self):
-        return f'<Plant {self.name} | In Stock: {self.is_in_stock}>'
+        return f'<Plant {self.id}: {self.name}>'
--- server/seed.py
+++ server/seed.py
@@ -3,11 +3,10 @@
 from app import app
 from models import db, Plant
 
-
 with app.app_context():
-
+    
     Plant.query.delete()
-
+    
     aloe = Plant(
         id=1,
         name="Aloe",
@@ -15,7 +14,7 @@ with app.app_context():
         price=11.50,
         is_in_stock=True,
     )
-
+    
     zz_plant = Plant(
         id=2,
         name="ZZ Plant",
@@ -23,6 +22,6 @@ with app.app_context():
         price=25.98,
         is_in_stock=False,
     )
-
+    
     db.session.add_all([aloe, zz_plant])
     db.session.commit()
